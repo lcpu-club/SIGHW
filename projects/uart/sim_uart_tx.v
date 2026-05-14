d6acfdcc6400154a5af34ebdeda4134608dd7047
Bohan Yu <ybh1998@librechip.com>
2026-05-14 17:00:01 +0800

    add course 4 project uart_tx vcd wave file

--- projects/uart/sim_uart_tx.v
+++ projects/uart/sim_uart_tx.v
@@ -16,26 +16,41 @@ uart_tx uart_tx_inst (
     .uart_tx(uart_tx)
 );
 
-
+// 64x clock frequency for 115200 baudrate
 always #67.817 clk = ~clk;
 
 initial begin
+    $dumpfile("sim_uart_tx.vcd");
+    $dumpvars(0, sim_uart_tx);
     clk = 0;
     rst_n = 0;
     data = 0;
     data_valid = 0;
     #200 rst_n = 1;
-    #200;
+    #10000;
+    // First character A
     @(posedge clk)
     #1 data = 8'h41;
     data_valid = 1;
+    // Second character B, data valid before ready 
     @(posedge clk)
     #1 data = 8'h42;
     @(posedge clk)
-    wait(data_ready == 1);
+    wait(data_ready == 1); // Wait for data consumption
     @(posedge clk)
     #1 data_valid = 0;
-    #100000 $finish;
+    wait(data_ready == 1); // Wait for transmit finish
+    #10000;
+    // Third character C, data valid after ready
+    @(posedge clk)
+    #1 data = 8'h43;
+    data_valid = 1;
+    @(posedge clk)
+    wait(data_ready == 1); // Wait for data consumption
+    @(posedge clk)
+    #1 data_valid = 0;
+    wait(data_ready == 1); // Wait for transmit finish
+    #10000 $finish;
 end
 
 endmodule
